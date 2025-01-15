824dccd5672f9647dc0857193212e5990c95768c
constantine1983 <constantineck83@gmail.com>
2025-01-15 19:32:34 +0200

    completed fo checking

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+const generateContacts = async (number) => {
+  const allContacts = await readContacts();
+  for (let index = 0; index < number; index++) {
+    allContacts.push(createFakeContact());
+  }
+
+  await writeContacts(allContacts);
+};
 
 generateContacts(5);
